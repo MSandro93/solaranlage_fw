0aaca00545049b121cb4d1ef1281ac7ad0a10342
Michael <->
2020-11-09 10:24:26 +0100

    init

---
+++ solaranlage_fw/main.c
@@ -0,0 +1,19 @@
+/*
+ * solaranlage_fw.c
+ *
+ * Created: 09.11.2020 10:22:20
+ * Author : stoff
+ */ 
+
+#include <avr/io.h>
+#include <iom644.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
